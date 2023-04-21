a4e7db0f6c3d6864d2ca69b94387669961c8964a
JhordanEdyvald <jhordan20112011@gmail.com>
2023-04-21 11:42:27 -0300

    First commit

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+//BOTÃO USUÁRIO DA NAVBAR
+$('#Conta-nav').click(()=>{
+    window.location.href = window.location.href;
+});
+
+//BOTÃO CARRINHO DA NAVBAR
+$('#Carrinho-nav').click(()=>{
+    window.location.href = window.location.href;
+});
---
+++ lib/navbar.php
@@ -0,0 +1,35 @@
+<?php
+    class navbar{
+        function __construct(){
+            echo "<link rel='stylesheet' href='../css/navbar.css'>";
+            echo '<nav>
+            <a class="link-logo" href="./index.php">
+                <div class="parte-logo"></div>
+            </a>
+            <div class="parteOptions">
+                <ul>
+                    <li>
+                        <a href="#">LENTES</a>
+                    </li> 
+                    <li>
+                        <a href="#">ARMAÇÕES</a>
+                    </li> 
+                    <li>
+                       <a href="#">CONTATOS</a>
+                   </li> 
+                   <li>
+                        <div class="container-cta-nav">
+                            <div class="cta-conta-user-nav" id="Conta-nav">
+    
+                            </div>
+                            <div class="cta-carrinho-nav" id="Carrinho-nav">
+    
+                            </div>
+                        </div>
+                   </li> 
+                </ul>
+            </div>
+        </nav>';
+        }
+    }
+?>
