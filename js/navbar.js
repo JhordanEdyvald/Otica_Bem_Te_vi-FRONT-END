10d18a49c783010755dd82b3d5e56e6cabb44a06
JhordanEdyvald <jhordan20112011@gmail.com>
2023-04-23 17:54:26 -0300

    Ultimas mudanças na home falta apenas o footer

--- js/navbar.js
+++ js/navbar.js
@@ -7,3 +7,30 @@ $('#Conta-nav').click(()=>{
 $('#Carrinho-nav').click(()=>{
     window.location.href = window.location.href;
 });
+
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+      const context = this;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+  function navbarEsquemaCor(){
+    const TopoJanela = window.pageYOffset + 77;
+    if(TopoJanela > 300){
+      $('nav').css('backgroundColor','#FF6C3B');
+    }else{
+      $('nav').css('backgroundColor','#0000');
+    }
+  }
+  
+$(this).scroll(debounce(()=>navbarEsquemaCor(),100));
+
--- lib/header.php
+++ lib/header.php
@@ -3,8 +3,12 @@ class header{
     function __construct(){
         echo "<link rel='stylesheet' href='../css/header.css'>";
         echo '<div class="header-container">
+        <div class="mensagem-container">
         <h1>Enxergue o mundo com clareza e estilo.<br> Descubra a variedade de opções em lentes e armações que temos para você na nossa ótica.</h1>
-        <div alt="" class="mulher-banner"></div>
+            <a type="button" href="./index.php" class="btn btn-warning">ARMAÇOES</a>
+        </div>
+        <div alt="" class="mulher-banner">
+        </div>
     </div>';
     }
 }
